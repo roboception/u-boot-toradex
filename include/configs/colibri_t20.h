d74c47c3647f9c11154e1d0bdcfabab819266d5c
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2015-08-18 10:51:00 +0200

    colibri_t20: implement early pmic rail configuration
    Implement early TPS6586X PMIC rail configuration setting SM0 being
    VDD_CORE_1.2V to 1.2 volts and SM1 being VDD_CPU_1.0V to 1.0 volts.
    
    While those are PMIC power-up defaults the SoC might have been reset
    separately with certain rails being left at lower DVFS states which
    is e.g. the case upon watchdog reset while otherwise nearly idling.
    
    (cherry picked from commit f7c3186985ebb244d075b04ed7c055f39f485670)

--- include/configs/colibri_t20.h
+++ include/configs/colibri_t20.h
@@ -14,6 +14,8 @@
 #undef CONFIG_SYS_DCACHE_OFF		/* breaks L4T kernel boot */
 #define CONFIG_ARCH_MISC_INIT
 
+#define CONFIG_TEGRA_EARLY_TPS6586X
+
 /* High-level configuration options */
 #define CONFIG_DISPLAY_BOARDINFO_LATE	/* Calls show_board_info() */
 
