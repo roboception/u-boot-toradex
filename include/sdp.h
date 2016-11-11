d7b22787ea3015270000d6b8bcb829c82fb9fc67
Stefan Agner <stefan.agner@toradex.com>
2016-11-11 09:59:42 -0800

    usb: gadget: add SDP driver
    Add SDP (Serial Downloader Protocol) implementation for U-Boot. The
    protocol is used in NXP SoCs boot ROM and allows to download program
    images. Beside that, it can also be used to read/write registers and
    download complete Device Configuration Data (DCD) sets. Currently,
    this implementation only supports downloading images and reading
    registers.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
    Acked-by: Max Krummenacher <max.krummenacher@toradex.com>

---
+++ include/sdp.h
@@ -0,0 +1,16 @@
+/*
+ * sdp.h - Serial Download Protocol
+ *
+ * Copyright (C) 2016 Toradex
+ * Author: Stefan Agner <stefan.agner@toradex.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __SDP_H_
+#define __SDP_H_
+
+int sdp_init(void);
+int sdp_handle(void);
+
+#endif /* __SDP_H_ */
