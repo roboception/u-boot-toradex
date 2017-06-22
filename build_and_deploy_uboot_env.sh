48127d1cb97e788fa406a8eec24756811bcb7b91
Florian Kromer <florian.kromer@roboception.de>
2017-06-22 14:12:09 +0200

    add extraction of def env to uboot env image script

--- build_and_deploy_uboot_env.sh
+++ build_and_deploy_uboot_env.sh
@@ -12,6 +12,10 @@ UBOOT_ENV_BIN=uboot-env.bin
 UBOOT_BIN_OUT_DIR=$SCRIPTPATH/../rc_visard_image/apalis-tk1_bin
 
 cd ${UBOOT_DIR}
+# extract the default environment from the u-boot source code
+# (is used as input to generate the u-boot environment image)
+./scripts/get_default_envs.sh > uboot-env.txt
+# generate the u-boot environment image from the extracted u-boot default environment
 ./tools/mkenvimage -r -s 0x20000 -o uboot-env.bin uboot-env.txt
 chmod 644 uboot-env.bin
 cp ${UBOOT_ENV_BIN} ${UBOOT_BIN_OUT_DIR}/${UBOOT_ENV_BIN}
