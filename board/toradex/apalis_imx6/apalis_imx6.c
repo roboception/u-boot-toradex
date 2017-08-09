d2bb682e0d114d6154c776f7ed378851d5966702
Stefan Agner <stefan.agner@toradex.com>
2017-08-08 18:23:24 -0700

    apalis_imx6: use SDP if USB serial downloader has been used
    In case USB serial downloader has been used to load U-Boot start the
    serial download protocol (SDP) emulation. This allows to download
    complete images such as Toradex Easy Installer over USB SDP as well.
    This code uses the boot ROM provided boot information to reliably
    detect USB serial downloader.

--- board/toradex/apalis_imx6/apalis_imx6.c
+++ board/toradex/apalis_imx6/apalis_imx6.c
@@ -837,6 +837,14 @@ int board_late_init(void)
 #endif /* CONFIG_TDX_APALIS_IMX6_V1_0 */
 #endif /* CONFIG_REVISION_TAG */
 
+#ifdef CONFIG_CMD_USB_SDP
+	if (is_boot_from_usb()) {
+		printf("Serial Downloader recovery mode, using sdp command\n");
+		setenv("bootdelay", "0");
+		setenv("bootcmd", "sdp 0");
+	}
+#endif /* CONFIG_CMD_USB_SDP */
+
 	return 0;
 }
 #endif /* CONFIG_BOARD_LATE_INIT */
